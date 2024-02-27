37c303294ee19df379c2ef8c0bcf8817c7cbae3f
Oleksandr Mosiiuk <mosxandrwork@gmail.com>
2024-02-27 21:31:26 +0200

    The component ShowItemList has been modified. Reworked using the ternary operator.

--- src/components/ShowItemList/index.js
+++ src/components/ShowItemList/index.js
@@ -1,14 +1,11 @@
  const ShowItemList = function(props){
-   if (props.mark < 90) {
-      return null;
-   }
-   else{
       return (<li>
          Name: {props.name}, 
          Group: {props.group}, 
-         Mark: {props.mark}
-      </li>)
-   }   
+         {Number(props.mark) < 90 ? 
+                 " Estimation less 90" : 
+                 " Estimation: " + props.mark}
+      </li>)  
  }
 
  export default ShowItemList;
